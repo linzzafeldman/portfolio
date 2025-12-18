c808d9efd2dc96ed03fe69015e15fa0c45ad1c09
linzzafeldman <linzza.feldman@gmail.com>
2025-12-18 13:41:32 +0200

    81

--- src/components/AnAtomy.tsx
+++ src/components/AnAtomy.tsx
@@ -50,7 +50,7 @@ export function AnAtomy() {
   ];
 
   return (
-    <div className="max-w-7xl mx-auto px-8">
+    <div className="max-w-5xl mx-auto px-8">
        <button
           // Теперь при клике мы просто меняем адрес на #projects
           onClick={() => window.location.hash = 'projects'}
