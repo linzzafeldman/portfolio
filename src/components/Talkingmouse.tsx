3afe4e1012d356b54a238850c693a57483f1e6ee
linzzafeldman <linzza.feldman@gmail.com>
2026-01-02 10:08:22 +0200

    115

--- src/components/Talkingmouse.tsx
+++ src/components/Talkingmouse.tsx
@@ -1,15 +1,15 @@
 import { Gallery } from "./Gallery";
 
-import artwork_set from "../media/twisters/Twisters-I_all-9.mp4";
+import artwork_set from "../images/projects/talkingmouse/OlgaFeldman_Mouse_red.webp";
 
 export function Talkingmouse() {
   const artworks = [
     {
       id: "art0",
       images: [artwork_set],
-      title: "Twisters I set of 9",
+      title: "Red Mouse",
       description:
-        "Twisters I set of 9",
+        "Initial artwork depicting the Talking Mouse. 2019",
     },
  ];
 
@@ -25,23 +25,15 @@ export function Talkingmouse() {
         <div className="mb-12">
           <h1 className="mb-4 tracking-wider">DECOMPOSITION OF THE TALKING MOUSE</h1>
           <p className="mb-2 opacity-60">
-            2009-2013 | 3D, Digital Art
+            2009-2013 | Digital Art
           </p>
           <div className="max-w-2xl">
             <p className="mt-6">
-              TTWISTERS is an original series of abstract monochrome animated artworks. </p>
-<p className="mt-6">It is a visual project about inconstant and changeable human nature, and about different ways people may react 
-to something that doesn’t fit their expectations. Based on a wordplay when the word «twist» can have several meanings. Each portrait represents an anthropomorphic torso contained of thousand layers, which became visible only when it twists: reacts to circumstances out of the frame.</p>
-<p className="mt-6">
-Once I’ve come to a dialogue, people were talking about silence, absence and lack of social presence.</p>
-<p>«Why don’t you talk to people anymore?», one asked.</p>
-<p>«Because I say something and people twist», said the other after a brief pause.
+              It was an accident. It wasn’t an accident. </p>
+<p>I came back home late at night. Exhausted. Empty. Desperate. Children has already slept. I was alone, overstressed and shattered. I turned on the computer and opened on of me art. 
+So bright, full of details and colors. Razzle red. Deep yellow. Misty violent. All at once. I felt sick. Selected the shapes. Switched to monochrome mode. Still, that wasn’t enough. Different shades of black (of white?) irritated me. I selected the shapes again and deleted all the color fills. Now it was what I needed. Silence. Quietness. Serenity. Ghost of an art. In other words, that was a start of depression.
 </p>
-            <p className="mt-6">The project dedicated to those who keeps silence.</p>
-            <p className="mt-6">TWISTERS contains 9 monochromatic sets. 
-Each set contains one animation in mp4 format and 2 still keyframes. The animation has no sound.
-Each set represents a state of a human personality reaction.
-</p>
+
           </div>
         </div>
         <Gallery artworks={artworks} />
